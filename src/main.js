a368579bb173dda84591f63d06c7fe096b8a2d5c
swyuliang <swyuliang@qq.com>
2025-06-18 18:55:57 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 加载Vant 核心组件
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+
+// 加载全局样式
+import './styles/index.less'
+
+// 加载动态设置REM基准值
+import 'amfe-flexible'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
