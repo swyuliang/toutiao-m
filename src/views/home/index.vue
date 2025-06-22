f9847198b907a34a37e2bc5d37633eb97d6f3465
Joe <36142847+swyuliang@users.noreply.github.com>
2025-06-22 20:30:12 +0800

    首页频道列表布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="home-container">
-    首页
-  </div>
+          <!-- 导航栏 -->
+      <van-nav-bar class="page-nav-bar">
+        <van-button
+          class="search-btn"
+          slot="title"
+          type="info"
+          size="small"
+          round
+          icon="search"
+        >搜索
+        </van-button>
+      </van-nav-bar>
+      <!-- /导航栏 -->
+      <!-- 频道列表 -->
+       <!--
+        通过v-model绑定当前激活标签对应的索引值，默认情况启用第一个标签
+        通过 animated 属性可以开启切换标签内容时的转场动画。
+        通过 swipeable 属性可以开启滑动切换标签页。
+       -->
+      <van-tabs class="channel-tabs" v-model="active" animated swipeable>
+        <van-tab title="推荐">推荐1</van-tab>
+        <van-tab title="热门话题">热门话题</van-tab>
+        <van-tab title="标签 3">标签 3</van-tab>
+        <van-tab title="标签 4">标签 4</van-tab>
+        <van-tab title="标签 4">标签 4</van-tab>
+        <van-tab title="标签 4">标签 4</van-tab>
+        <van-tab title="标签 4">标签 4</van-tab>
+        <van-tab title="标签 4">标签 4</van-tab>
+        <div slot="nav-right" class="placeholder"></div>
+        <div slot="nav-right" class="hamburger-btn">
+          <i class="toutiao toutiao-gengduo"></i>
+        </div>
+      </van-tabs>
+      <!-- /频道列表 -->
+    </div>
 </template>
 
 <script>
@@ -10,7 +43,7 @@ export default {
   component: {},
   props: {},
   data () {
-    return {}
+    return { active: 0 }
   },
   created () {},
   mounted () {},
@@ -18,4 +51,71 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    i.van-icon {
+     font-size: 32px;
+    }
+   }
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      min-width: 200px;
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0px;
+    }
+    .van-tabs__line {
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+      bottom: 8px;
+    }
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 80px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 80px;
+      background-color: #fff;
+      opacity: 0.902;
+      i.toutiao {
+        font-size: 33px;
+      }
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 100%;
+        background-color: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
+    }
+  }
+}
+</style>
