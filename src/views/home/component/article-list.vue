6c575c7d5b10c7ba5c70d5595785c9228b46bed5
Joe <36142847+swyuliang@users.noreply.github.com>
2025-06-26 21:37:58 +0800

    文章列表项布局

--- src/views/home/component/article-list.vue
+++ src/views/home/component/article-list.vue
@@ -31,11 +31,16 @@
         error-text="请求失败，点击重新加载"
         @load="onLoad"
       >
-        <van-cell
+        <article-item
+          v-for="(article, index) in list"
+          :key="index"
+          :article="article"
+        />
+        <!-- <van-cell
           v-for="(article, index) in list"
           :key="index"
           :title="article.title"
-        />
+        /> -->
       </van-list>
     </van-pull-refresh>
   </div>
@@ -43,10 +48,13 @@
 
 <script>
 import { getArticles } from '@/api/article'
+import ArticleItem from '@/compoments/article-item'
 
 export default {
   name: 'ArticleList',
-  components: {},
+  components: {
+    ArticleItem
+  },
   props: {
     channel: {
       type: Object,
@@ -155,4 +163,10 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.article-list {
+  // 百分比单位是相对于父元素的
+  height: 79vh;
+  overflow-y: auto;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
           <!-- 导航栏 -->
-      <van-nav-bar class="page-nav-bar">
+      <van-nav-bar class="page-nav-bar" fixed>
         <van-button
           class="search-btn"
           slot="title"
@@ -81,6 +81,7 @@ export default {
 
 <style scoped lang="less">
 .home-container {
+  padding-top: 174px;
   padding-bottom: 100px;
   .van-nav-bar__title {
     max-width: unset;
@@ -97,6 +98,11 @@ export default {
    }
   /deep/ .channel-tabs {
     .van-tabs__wrap {
+      position: fixed;
+      top: 92px;
+      z-index: 1;
+      left: 0;
+      right: 0;
       height: 82px;
     }
     .van-tab {
